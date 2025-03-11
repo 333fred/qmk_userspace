77b5a18776c7105264213e1ade320d3236c0ee08
Fredric Silberberg <fred@silberberg.xyz>
2025-03-10 20:07:34 -0700

    Modification for basic compilation. Only size issue is now the manuform

--- users/333fred/333fred.h
+++ users/333fred/333fred.h
@@ -49,3 +49,5 @@ void tap_dance_sym_vim_finished(tap_dance_state_t*, void*);
 void tap_dance_sym_vim_reset(tap_dance_state_t*, void*);
 void tap_dance_process_keycode(uint16_t);
 bool try_handle_macro(uint16_t keycode, keyrecord_t *record);
+
+extern tap_dance_action_t tap_dance_actions[2];
